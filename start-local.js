afc82b92736f2575a30da1041d8f1f15ccd809d7
ergashevv <ergashevpulot@gmail.com>
2026-04-08 13:02:42 +0500

    das

--- start-local.js
+++ start-local.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 const app = require("./server");
+const bot = require("./bot");
 
 const port = Number(process.env.PORT || 3000);
 
 app.listen(port, () => {
   console.log(`Server ishga tushdi: http://localhost:${port}`);
+
+  if (process.env.BOT_TOKEN) {
+    bot.launch().then(() => {
+      console.log("Telegram bot ishga tushdi (polling rejim).");
+    }).catch((err) => {
+      console.error("Bot ishga tushmadi:", err.message);
+    });
+  }
 });
+
+process.once("SIGINT", () => { bot.stop("SIGINT"); process.exit(); });
+process.once("SIGTERM", () => { bot.stop("SIGTERM"); process.exit(); });
